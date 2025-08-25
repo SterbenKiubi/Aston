61a30b2877eb54d5e73ed28ba2af153fcc462df0
Sterben <sergey.matushkin.98@mail.ru>
2025-08-25 11:49:56 +0300

    feat: added data placeholder

---
+++ my-post-app/src/shared/api/api.js
@@ -0,0 +1,20 @@
+export const mockPosts = [
+    {
+        userId: 1,
+        id: 1,
+        title: 'title1',
+        body: 'body text 1'
+    },
+    {
+        userId: 1,
+        id: 2,
+        title: 'title2',
+        body: 'body text 2'
+    },
+    {
+        userId: 2,
+        id: 3,
+        title: 'title1',
+        body: 'body text 3'
+    },
+];
