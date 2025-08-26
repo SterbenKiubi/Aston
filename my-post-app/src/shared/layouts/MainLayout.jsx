d66220a019cebf83558ca74ae2039930b86b94e9
Sterben <sergey.matushkin.98@mail.ru>
2025-08-26 18:03:36 +0300

    feat: add header and main layout

---
+++ my-post-app/src/shared/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../widgets/LayoutHeader/Header";
+
+const MainLayout = () => {
+    return (
+        <div className="layout">
+            <Header />
+        </div>
+    )
+}
+
+export default MainLayout;
