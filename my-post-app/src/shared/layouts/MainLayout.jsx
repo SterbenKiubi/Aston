8db701471417227042670568ac04bb6c487a4bf0
Sterben <sergey.matushkin.98@mail.ru>
2025-08-26 18:07:32 +0300

    feat: add footer

--- my-post-app/src/shared/layouts/MainLayout.jsx
+++ my-post-app/src/shared/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../widgets/LayoutHeader/Header";
+import Footer from "../../widgets/LayoutFooter/Footer";
 
 const MainLayout = () => {
     return (
         <div className="layout">
             <Header />
+            <Footer />
         </div>
     )
 }
