2ed74314e2303bfd5b9c3cc0c7ebe64c94c466e5
Sterben <sergey.matushkin.98@mail.ru>
2025-09-03 12:48:26 +0300

    feat: add button

--- my-post-app/src/widgets/LayoutHeader/Header.tsx
+++ my-post-app/src/widgets/LayoutHeader/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from '../../shared/ui/Button/Button';
 
 const Header: React.FC = () => {
     return (
         <header className="header">
             <h1>Приложение с постами</h1>
+            <nav>
+                <Button />
+            </nav>
         </header>
     );
 };
